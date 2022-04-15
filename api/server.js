4b3a8bd0aa0b8d5dacf877a8a93de18fd0858a1a
Andre Blaze <kuraiokamiblaze@gmail.com>
2022-04-14 18:25:28 -0600

    API fully fleshed out_ready for deployment

--- api/server.js
+++ api/server.js
@@ -11,15 +11,53 @@ server.get('/', (req, res) => {
     res.status(200).json('Welcome!');
 })
 
-server.get('/users', (req, res, next) => {
+server.get('/users', (req, res) => {
     Users.findAll()
         .then(users => {
-            res.status(200).json(users)
+           res.status(200).json(users)
         }) 
         .catch(err => {
             console.log(err)
             res.status(500).json('Users could not be retrieved')
         })
+})
+
+server.post('/register', (req, res) => {
+    const { userName, password } = req.body
+    if(!userName || !password){
+        res.status(400).json("userName and password required to register")
+    } else {
+        Users.create(req.body)
+            .then(user => {
+                res.status(201).json(user)
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json("New user could not be registered")
+            })
+    }
+})
+
+server.post('/login', (req, res) => {
+    const userName = req.body.userName.toUpperCase();
+    const password = req.body.password;
+
+    if(!req.body.userName || !password){
+        res.status(400).json("userName and password required to register");
+    } else {
+        Users.validate({ userName, password })
+            .then(user => {
+                if(!user){
+                    res.status(404).json("userName or password is invalid")
+                } else{
+                    res.status(200).json(`Welcome ${user.userName}`)
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json("New user could not be registered")
+            })
+    }
 })
 
 module.exports = server
--- api/users-model.js
+++ api/users-model.js
@@ -5,13 +5,35 @@ function getId() {
 }
 
 let users = [
-    {id: getId(), name: "Ace"},
-    {id: getId(), name: "Bonnie"},
-    {id: getId(), name: "Clyde"},
+    {id: getId(), userName: "ACE", password: "*Flame" },
+    {id: getId(), userName: "BONNIE", password: "+Love" },
+    {id: getId(), userName: "CLYDE", password: "#Guns"},
 ]
 
 module.exports = {
     async findAll() {
-        return users
+        let usersDisplay = [];
+        users.map(user => {
+            usersDisplay.push({ id: user.id, userName: user.userName });
+        })
+        return usersDisplay;
+    },
+
+    async create({ userName, password }){
+        const newUser = { id: getId(), userName: userName.toUpperCase(), password };
+        users.push(newUser);
+        return newUser;
+    },
+
+    async validate({ userName, password }){
+        const validUser = users.find(user => user.userName == userName);
+        
+        if(!validUser){
+            return null
+        } else if (validUser.password !== password) {
+            return null
+        } else {
+            return validUser
+        }
     }
 }
